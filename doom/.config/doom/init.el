0c0a7213770d83386e67375b0105f62b4b8fe8fa
keithhauzh <keithhau.thegreat@gmail.com>
2026-01-19 22:05:18 +0800

    eesh lots of stuff were updated, its basically a new config

--- doom/.config/doom/init.el
+++ doom/.config/doom/init.el
@@ -159,7 +159,7 @@
        ;;nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +roam)         ; organize your plain life in plain text
-       ;;php               ; perl's insecure younger brother
+       php                 ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;graphviz          ; diagrams for confusing yourself even more
        ;;purescript        ; javascript, but functional
